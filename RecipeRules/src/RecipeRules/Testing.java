0f93b8985fbee85ebdccb707d14f4e414d947ca6
Jakob Siemens <siemensj@mail.upb.de>
2018-06-06 15:45:40 +0200

    Add Xtext Project, Add Testing Classes for Patterns

---
+++ RecipeRules/src/RecipeRules/Testing.java
@@ -0,0 +1,23 @@
+package RecipeRules;
+
+import RecipeRules.api.RecipeRulesAPI;
+import RecipeRules.api.RecipeRulesDemoclesApp;
+
+import org.emoflon.ibex.gt.democles.runtime.DemoclesGTEngine;
+import org.junit.Before;
+
+abstract public class Testing extends RecipeRulesDemoclesApp {
+	protected  RecipeRulesAPI api;
+	protected static final String INSTANCES = "/RecipeLanguage/instances";
+
+	@Before
+	public void setup() throws Exception {
+		DemoclesGTEngine engine = new DemoclesGTEngine();
+		resourceSet = engine.createAndPrepareResourceSet(workspacePath);
+
+		loadModels();
+		api = initAPI(engine);
+	}
+
+	abstract protected void loadModels();
+}
